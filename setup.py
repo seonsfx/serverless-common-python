e4f4989a3a2b18a3b01dd4916194a737deaf574c
Seon Kim <skim@signalfx.com>
2019-04-02 09:49:22 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     version=version,  # noqa
     author='SignalFx, Inc',
     author_email='info@signalfx.com',
-    description='SignalFx https://github.com/signalfx/serverless-common-python Python Wrapper',
+    description='SignalFx https://github.com/seonsfx/serverless-common-python Python Wrapper',
     license='Apache Software License v2',
     long_description=long_description,
     long_description_content_type='text/x-rst',
@@ -29,5 +29,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    url='https://github.com/signalfx/serverless-common-python',
+    url='https://github.com/seonsfx/serverless-common-python',
 )
