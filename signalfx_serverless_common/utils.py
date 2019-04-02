928a4657a8f322776744a6ddfd89f735de82ef51
Seon Kim <skim@signalfx.com>
2019-04-02 12:47:45 -0400

    Add set_fields

--- signalfx_serverless_common/utils.py
+++ signalfx_serverless_common/utils.py
@@ -7,22 +7,11 @@ fields = {}
 dim_prefix = 'gcf'
 
 def get_fields():
-    env_dims = {
-        'FUNCTION_REGION': dim_prefix + '_region',
-        'GCP_PROJECT': dim_prefix + '_project_id',
-        'FUNCTION_NAME': dim_prefix + '_function_name',
-        'X_GOOGLE_FUNCTION_VERSION': dim_prefix + '_function_version'
-    }
-
-    for env_name, dim in env_dims.items():
-        runtime_env = os.environ.get(env_name)
-        if env_name is not None:
-            fields[dim] = runtime_env
-    
-    fields['function_wrapper_version'] = name + '_' + version
-
     return fields.copy()
 
+def set_fields(fs):
+    for key, value in fs.items():
+        fields[key] = value
 
 def get_metrics_url():
     url = os.environ.get('SIGNALFX_INGEST_ENDPOINT')
